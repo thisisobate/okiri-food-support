8dab0e0d9889ee74f2410842efd17850412f5315
thisisobate <obasiuche62@gmail.com>
2023-03-14 01:16:53 +0100

    init
    Signed-off-by: thisisobate <obasiuche62@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Chat } from './components/Chat';
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Okiri food support</h1>
+      <Chat />
+    </div>
+  )
+}
+
+export default App
